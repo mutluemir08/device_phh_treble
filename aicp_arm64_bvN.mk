f6d96b37aeb0f7801423914be06c380dccfe9aac
Mutlu Öztürk <49791081+mutluemir08@users.noreply.github.com>
2020-12-04 12:50:32 +0300

    Create aicp_arm64_bvN.mk

---
+++ aicp_arm64_bvN.mk
@@ -0,0 +1,15 @@
+$(call inherit-product, device/phh/treble/base-pre.mk)
+include build/make/target/product/aosp_arm64_ab.mk
+$(call inherit-product, device/phh/treble/base.mk)
+
+# Inherit some common Nusantara stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
+
+PRODUCT_NAME := aicp_arm64_bvN
+PRODUCT_DEVICE := phhgsi_arm64_ab
+PRODUCT_BRAND := Android
+PRODUCT_MODEL := Phh-Treble vanilla
+
+
+
+PRODUCT_PACKAGES += 
